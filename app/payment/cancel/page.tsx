cfd83eb11a02a8a67f9d5551964b8651e59c692e
Julian-Kellmer <kellmer002@gmail.com>
2026-01-10 17:55:56 -0300

    Fixed fxlink button in payments page

--- app/payment/cancel/page.tsx
+++ app/payment/cancel/page.tsx
@@ -1,11 +1,12 @@
-import FxButton from '../../components/FxButton/FxButton'
+'use client'
+import {FxLinkButton} from '../../components/FxButton/FxLinkButton'
 
 export default function PaymentCancelPage() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen text-center p-4'>
       <h1 className='text-4xl font-bold mb-4 uppercase'>Payment Cancelled</h1>
       <p className='text-xl mb-8'>The transaction was not completed.</p>
-      <FxButton href='/'>Return to Home</FxButton>
+      <FxLinkButton href='/'>Return to Home</FxLinkButton>
     </main>
   )
 }
